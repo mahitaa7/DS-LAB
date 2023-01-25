42b00b7cacae964bd879efd076b1f53d73d0ee4f
mahitaa7 <111947588+mahitaa7@users.noreply.github.com>
2023-01-25 14:39:12 +0530

    Add files via upload

--- selection.c
+++ selection.c
@@ -1,12 +1,39 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int main()
 {
-	int i,j,temp,n,a[20];
-	printf("enter the size of array:");
-	scanf("%d",&n);
-	printf("enter the elements of array:");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
+  int array[100], n, c, d, position, t;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+
+  for (c = 0; c < (n - 1); c++)
+  {
+    position = c;
+
+    for (d = c + 1; d < n; d++)
+    {
+      if (array[position] > array[d])
+        position = d;
+    }
+    if (position != c)
+    {
+      t = array[c];
+      array[c] = array[position];
+      array[position] = t;
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (c = 0; c < n; c++)
+    printf("%d\n", array[c]);
+
+  return 0;
+}
+
 
