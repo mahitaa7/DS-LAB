94430beb4f11f368b4deeb2b46e0b79f0903b7c2
mahitaa7 <111947588+mahitaa7@users.noreply.github.com>
2023-05-04 20:18:10 +0530

    Update bubble.c

--- bubble.c
+++ bubble.c
@@ -2,15 +2,11 @@
 void main()
 {
   int array[100], n, c, d, swap;
-
   printf("Enter number of elements\n");
   scanf("%d", &n);
-
   printf("Enter %d integers\n", n);
-
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
-
   for (c = 0 ; c < n - 1; c++)
   {
     for (d = 0 ; d < n - c - 1; d++)
@@ -23,12 +19,9 @@ void main()
       }
     }
   }
-
   printf("Sorted list in ascending order:\n");
-
   for (c = 0; c < n; c++)
      printf("%d\n", array[c]);
-
   return 0;
 }
 
