681539ab3402cc529db649acd2e17340786d2cbc
mahitaa7 <111947588+mahitaa7@users.noreply.github.com>
2023-05-04 20:23:55 +0530

    Update selection.c

--- selection.c
+++ selection.c
@@ -2,19 +2,14 @@
 int main()
 {
   int array[100], n, c, d, position, t;
-
   printf("Enter number of elements\n");
   scanf("%d", &n);
-
   printf("Enter %d integers\n", n);
-
   for (c = 0; c < n; c++)
     scanf("%d", &array[c]);
-
   for (c = 0; c < (n - 1); c++)
   {
     position = c;
-
     for (d = c + 1; d < n; d++)
     {
       if (array[position] > array[d])
@@ -27,12 +22,9 @@ int main()
       array[position] = t;
     }
   }
-
   printf("Sorted list in ascending order:\n");
-
   for (c = 0; c < n; c++)
     printf("%d\n", array[c]);
-
   return 0;
 }
 
